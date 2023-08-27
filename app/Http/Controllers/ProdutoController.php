03fdb8b9e865d311ad4314059b0583ee5ade839b
lucianobcorrea <lucianobalestrincorrea@gmail.com>
2023-08-27 14:04:57 -0300

    adicionando arquivo JSON do Postman e adicionando descrição no método atualizar

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -30,24 +30,48 @@ class ProdutoController extends Controller
 
     public function cadastrar(Request $request)
     {
+        $mensagensDeErro = [
+            'nome.min' => 'O nome deve ter pelo menos :min caracteres.',
+            'nome.max' => 'O nome não pode ter mais que :max caracteres.',
+            'descricao.min' => 'A descrição deve ter pelo menos :min caracteres.',
+            'descricao.max' => 'A descrição não pode ter mais que :max caracteres.',
+            'preco.required' => 'O preço é obrigatório.',
+            'preco.gt' => 'O preço deve ser maior que zero.',
+            'quantidade.required' => 'A quantidade é obrigatória.',
+            'quantidade.gt' => 'A quantidade deve ser maior que zero.',
+        ];
+
         $request->validate([
             'nome' => 'min:3|max:100',
             'descricao' => 'min:3|max:512',
             'preco' => 'required|gt:0',
             'quantidade' => 'required|gt:0'
-        ]);
+        ], $mensagensDeErro);
 
         Produto::create($request->all());
     }
 
-    public function cadastrarProdutoView() {
+    public function cadastrarProdutoView()
+    {
         return view('site.cadastrar-produto');
     }
 
     public function atualizar(Request $request, Produto $produto)
     {
+        $mensagensDeErro = [
+            'nome.min' => 'O nome deve ter pelo menos :min caracteres.',
+            'nome.max' => 'O nome não pode ter mais que :max caracteres.',
+            'descricao.min' => 'A descrição deve ter pelo menos :min caracteres.',
+            'descricao.max' => 'A descrição não pode ter mais que :max caracteres.',
+            'preco.required' => 'O preço é obrigatório.',
+            'preco.gt' => 'O preço deve ser maior que zero.',
+            'quantidade.required' => 'A quantidade é obrigatória.',
+            'quantidade.gt' => 'A quantidade deve ser maior que zero.',
+        ];
+
         $request->validate([
             'nome' => 'min:3|max:100',
+            'descricao' => 'min:3|max:512',
             'preco' => 'required|gt:0',
             'quantidade' => 'required|gt:0'
         ]);
